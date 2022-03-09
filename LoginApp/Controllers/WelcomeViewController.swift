b4f466711cc281a631b383548370c33dc5f85da5
dimawko <sagyntkan@gmail.com>
2022-03-10 04:34:49 +0600

    Add gradient view's background color

--- LoginApp/Controllers/WelcomeViewController.swift
+++ LoginApp/Controllers/WelcomeViewController.swift
@@ -10,9 +10,17 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [UIColor.cyan.cgColor, UIColor.purple.cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x : 0.0, y : 0)
+        gradient.endPoint = CGPoint(x :0.0, y: 0.7)
+        gradient.frame = view.bounds
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
         welcomeLabel.text = welcomeText
     }
-
+    
     @IBAction func logOutButton() {
         dismiss(animated: true)
     }
