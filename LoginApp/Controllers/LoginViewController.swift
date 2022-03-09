d65922bdb2c60e354d53ddb8c5000811a7ef3d48
dimawko <sagyntkan@gmail.com>
2022-03-10 01:35:29 +0600

    Fix links in main.storyboard, add alert methods to forgotUserName and forgotPassword buttons

--- LoginApp/Controllers/LoginViewController.swift
+++ LoginApp/Controllers/LoginViewController.swift
@@ -16,15 +16,36 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func logInButtonPressed() {
-        
+        userNameTF.text = ""
+        passwordTF.text = ""
     }
     
     @IBAction func forgotUserNameButtonPressed() {
         
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your name is Dimash 😉",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
     
     @IBAction func forgotPasswordButtonPressed() {
         
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your password is 12345 😉",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
